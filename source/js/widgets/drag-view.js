2717551e72437b29ef38b5a36110bb58233c40b4
Kevin <kevinlu1736@gmail.com>
2021-05-10 14:56:55 -0700

    wip

---
+++ source/js/widgets/drag-view.js
@@ -0,0 +1,23 @@
+class DragView extends HTMLDivElement {
+    constructor() {
+        super();
+
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <div class="drag-frame">
+
+        </div>
+        `;
+
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+
+    
+
+
+    
+
+}
+
+customElements.define('drag-view', DragView)
