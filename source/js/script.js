cf9beec325036f4822b40fedaa71fe0db4a68d28
Ishaan Gupta <ishaangupta@Ishaans-MacBook-Pro-2.local>
2021-05-15 10:28:58 -0700

    Page in Shadow

---
+++ source/js/script.js
@@ -0,0 +1,5 @@
+
+page = document.createElement("drag-view");
+
+console.log(page)
+document.getElementsByTagName("main")[0].appendChild(page);
--- source/js/widgets/drag-view.js
+++ source/js/widgets/drag-view.js
@@ -1,15 +1,29 @@
-class DragView extends HTMLDivElement {
+class DragView extends HTMLElement {
   constructor() {
     super()
-
+    console.log('const called')
     const template = document.createElement('template')
     template.innerHTML = `
         <div class="drag-frame">
-
+          <div class="page" contenteditable="true">
+            <h1>New Notes</h1>
+            <ol>
+              <li>Notes 1</li>
+            </ol>
+            <br />
+            <ul>
+              <li>Notes 2</li>
+            </ul>
+          </div>
         </div>
         `
 
+    const link = document.createElement('link')
+    link.setAttribute('rel', 'stylesheet')
+    link.setAttribute('href', 'style.css')
+
     this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(link)
     this.shadowRoot.appendChild(template.content.cloneNode(true))
   }
 }
