5414e40bcb7961fc48dab90d6a83da63c41f48f4
Ishaan Gupta <ishaangupta@Ishaans-MacBook-Pro-2.local>
2021-05-15 20:42:36 -0700

    Styling

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,7 @@
 page = document.createElement('drag-view')
 document.getElementsByTagName('main')[0].appendChild(page)
 
-// not sure if `toolselected` should be here or in one of the components
-// so for now Ishaan added this in both
-var toolselected = 'text'
-var indent = 2
 function openIndex() {
   console.log(document.querySelector('.left-pane'))
   document.querySelector('.left-pane').style.height = '100px'
 }
-
