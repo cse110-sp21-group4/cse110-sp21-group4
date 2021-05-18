ea160efcce934572d6cd933d46fcd4232b50a516
rsingh84 <77314754+rsingh84@users.noreply.github.com>
2021-05-17 21:02:48 -0700

    changed width

--- source/js/main-page-controller.js
+++ source/js/main-page-controller.js
@@ -1,10 +1,16 @@
 export class MainPageController {
   constructor(view) {
     this.view = view
+    
   }
 
   //Example code
   onTasksButtonClicked() {
     console.log('Tasks button clicked...')
   }
+
+
+
+
+
 }
