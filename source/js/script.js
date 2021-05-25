0083856c52a29a871ef1b6160bb28210bf59edb1
ZhongKang Fang <z4fang@ucsd.edu>
2021-05-25 15:14:40 -0700

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -3,9 +3,11 @@ import { MainPageController } from './main-page-controller.js'
 
 let page = document.createElement('drag-view')
 let left = document.createElement('left-pane')
+let right = document.createElement('right-pane')
 
 document.getElementsByTagName('main')[0].appendChild(page)
 document.getElementsByTagName('main')[0].appendChild(left)
+document.getElementsByTagName('main')[0].appendChild(right)
 
 var toolselected = 'text'
 
