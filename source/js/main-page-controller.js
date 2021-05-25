b6b7731ac71f328e49fa12205e15cee43cc8f88c
Kevin <kevinlu1736@gmail.com>
2021-05-25 15:05:51 -0700

    merge drag-and-drop with left-pane

--- source/js/main-page-controller.js
+++ source/js/main-page-controller.js
@@ -1,15 +1,58 @@
 export class MainPageController {
   constructor(view) {
     this.view = view
-    registerListeners()
+    
+  
   }
 
   registerListeners() {
-    document.querySelector('').addEventListener('', () => {})
+    document.querySelector('left-pane').shadowRoot.querySelector("#left-pane-button").addEventListener("click", () => {    
+      var v = document.querySelector("left-pane").shadowRoot.querySelector('#left-pane');
+      var p = document.querySelector('drag-view').shadowRoot.querySelector(".page");
+      if (v.style.display === "none") {
+         v.style.display = "block";
+         p.style.left = "429px";
+      } else {
+        // Page hides - Move MAIN page left
+         v.style.display = "none";
+         p.style.left = "429px";
+      }
+      
+    })
+
+    var myNodelist = document.querySelector('left-pane').shadowRoot.querySelectorAll("LI");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7"); // x symbol
+      span.className = "close";
+      span.appendChild(txt);
+      myNodelist[i].appendChild(span);
+    }
+
+    // Click on a close button to hide the current list item
+    var close = document.querySelector('left-pane').shadowRoot.querySelectorAll("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+
+
+
+
+
+    // add Javascript code here
+    
   }
 
   //Example code
   onTasksButtonClicked() {
     console.log('Tasks button clicked...')
   }
+
+
+
 }
