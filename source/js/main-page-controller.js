85e41f997e22588739c63b4ab0908c6a0a6742ba
Ishaan Gupta <ishaangupta@Ishaans-MacBook-Pro-2.local>
2021-05-27 16:45:13 -0700

    before

--- source/js/main-page-controller.js
+++ source/js/main-page-controller.js
@@ -9,14 +9,10 @@ export class MainPageController {
     console.log(
       document.querySelector('tool-bar').shadowRoot.querySelector('#text-tool')
     )
-    // Toolbar buttons
-    let textTool = document
-      .querySelector('tool-bar')
-      .shadowRoot.querySelector('#text-tool')
 
-    textTool.addEventListener('click', (event) => {
-      if (textTool.classList.contains('selected-tool')) {
-        textTool.classList.remove('selected-tool')
+    function selectTool(toolToBeSelected) {
+      if (toolToBeSelected.classList.contains('selected-tool')) {
+        toolToBeSelected.classList.remove('selected-tool')
       } else {
         if (
           document
@@ -30,8 +26,16 @@ export class MainPageController {
         } else {
           console.log('No tool selected')
         }
-        textTool.classList.add('selected-tool')
+        toolToBeSelected.classList.add('selected-tool')
       }
+    }
+    // Toolbar buttons
+    let textTool = document
+      .querySelector('tool-bar')
+      .shadowRoot.querySelector('#text-tool')
+
+    textTool.addEventListener('click', (event) => {
+      selectTool(textTool)
 
       let dragView = document.querySelector('drag-view')
       console.log(dragView)
@@ -47,32 +51,34 @@ export class MainPageController {
 
     imageTool.addEventListener('click', (event) => {
       console.log('You just clicked image tool')
-      if (imageTool.classList.contains('selected-tool')) {
-        imageTool.classList.remove('selected-tool')
-      } else {
-        if (
-          document
-            .querySelector('tool-bar')
-            .shadowRoot.querySelector('.selected-tool')
-        ) {
-          let selectedTool = document
-            .querySelector('tool-bar')
-            .shadowRoot.querySelector('.selected-tool')
 
-          console.log('You had selected', selectedTool)
-
-          selectedTool.classList.remove('selected-tool')
-        } else {
-          console.log('No tool selected')
-        }
-        imageTool.classList.add('selected-tool')
-      }
+      selectTool(imageTool)
 
       // let imageView = document.querySelector('image-view')
       // console.log(imageView)
     })
-  }
+  
+  // bullets tool
+  let bulletsTool = document
+      .querySelector('tool-bar')
+      .shadowRoot.querySelector('#bullet-tool')
 
+      bulletsTool.addEventListener('click', (event) => {
+      selectTool(bulletsTool)
+
+      let dragView = document.querySelector('drag-view')
+      console.log(dragView)
+      console.log(dragView.textOnClick)
+      dragView.textOnClick = dragView.textOnClick ? false : true
+    })
+
+
+
+
+
+
+
+  }
   //Example code
   onTasksButtonClicked() {
     console.log('Tasks button clicked...')
